f6fbb0997662860e2e92916ddf5fc9b068ddb288
ioannis <ioannis@easymarkets.com>
2021-06-11 08:01:37 +0300

    add new db operations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbOper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -8,23 +9,27 @@ MongoClient.connect(url, (err, client) => {
     assert.equal(err, null);
     console.log('Connected successfully to the server');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
+    
+    dbOper.insertDocument(db, { name: "Vadonut", description: 'Test'}, 'dishes', (result) => {
+        console.log('Insert Document: \n', result.ops);
 
-    collection.insert({"name": 'uthapizza', "description": "test"}, (err, result) => {
-        assert.equal(err, null);
+        dbOper.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found the documents \n' + docs);
+            dbOper.updateDocument(db, {name: 'Vadonut'}, {description: 'Update Value test'}, 'dishes', (result) => {
+                console.log('update document \n', result.result.n);
 
-        console.log('After Insert: \n');
-        console.log(result.ops);
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
-            console.log('Found:\n');
-            console.log(docs);
+                dbOper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found the documents \n' + docs);
 
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null);
-                client.close();
-            })
-        });
-    })
+                    db.dropCollection('dishes', (result) => {
+                        console.log('dropped collection: ', result);
+
+                        client.close();
+                    })
+                });
+            });
+        })
+
+    });
 });
 
